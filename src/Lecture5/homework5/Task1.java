84a1c74e5ad07649fdfc69704b4daa97b08fb989
Tanya Sheytanova <sheytanova.t@gmail.com>
2025-08-09 15:17:44 +0300

    live demos lecture 5 added

---
+++ src/Lecture5/homework5/Task1.java
@@ -0,0 +1,19 @@
+package Lecture5.homework5;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        int[] numbers = {134,69,85};
+        smallestNumber(numbers);
+    }
+
+    public static void smallestNumber(int[] numbers){
+        int smallestNumber = numbers[0];
+        for(int number : numbers){
+            if (smallestNumber > number){
+                smallestNumber = number;
+            }
+        }
+        System.out.println("The smallest number is: " + smallestNumber);
+    }
+}
---
+++ src/Lecture5/homework5/Task8.java
@@ -0,0 +1,23 @@
+package Lecture5.homework5;
+
+public class Task8 {
+    public static void main(String[] args) {
+        int numberOne = 22;
+        int numberTwo = 5;
+
+        System.out.println("Is " + numberOne +" even? " + booleanToText(isEven(numberOne)));
+        System.out.println("Is " + numberTwo +" even? " + booleanToText(isEven(numberTwo)));
+    }
+
+    public static boolean isEven(int number){
+        return number % 2 == 0;
+    }
+    public static String booleanToText(boolean boolStatement){
+        if (boolStatement){
+            return "Yes";
+        }
+
+        return "No";
+    }
+
+}
